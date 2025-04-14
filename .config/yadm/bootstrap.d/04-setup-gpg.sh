b32232794879b4a126eca37cdddc45c598e92efc
Jacob L <lecoqjacob@gmail.com>
2025-04-13 20:27:16 -0500

    chore: more linux fixes; add astronvim sub directory

--- .config/yadm/bootstrap.d/04-setup-gpg.sh
+++ .config/yadm/bootstrap.d/04-setup-gpg.sh
@@ -2,6 +2,6 @@
 
 echo "Setting up gpg keys..."
 
-yadm decrypt
+#yadm decrypt
 gpg --import "$HOME/.local/share/gnupg/.exported-keyring"/*.asc
 gpg --import-ownertrust "$HOME/.local/share/gnupg/.exported-keyring"/ownertrust.txt
