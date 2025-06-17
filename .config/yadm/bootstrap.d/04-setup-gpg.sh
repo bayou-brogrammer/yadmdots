e8d65f90ff44634b99af321a63d098ad86437e02
Jacob L <lecoqjacob@gmail.com>
2025-06-17 16:04:31 -0500

    chore: personal changes

--- .config/yadm/bootstrap.d/04-setup-gpg.sh
+++ .config/yadm/bootstrap.d/04-setup-gpg.sh
@@ -2,6 +2,12 @@
 
 echo "Setting up gpg keys..."
 
+brew install gnupg
+
+mkdir -p ~/.local/share/gnupg
+mkdir -p ~/.local/share/gnupg/.exported-keyring
+yadm decrypt
+
 #yadm decrypt
 gpg --import "$HOME/.local/share/gnupg/.exported-keyring"/*.asc
 gpg --import-ownertrust "$HOME/.local/share/gnupg/.exported-keyring"/ownertrust.txt
